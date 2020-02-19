f5e5bc9dd1318f5b0d33c75076f3df691c9f6a66
AjxLab <abe12@mccc.jp>
2020-02-19 17:52:06 +0900

    音声のサンプリングレート・秒数を変換

--- voice_similarity/engine.py
+++ voice_similarity/engine.py
@@ -12,7 +12,19 @@ def cos_sim(v1, v2):
 
     return sum(sim) / len(sim)
 
+def convert_wave(ori_file, to_file, fs, sec):
+    ## -----*----- 音声ファイルのサンプリングレート・時間を変換 -----*----- ##
+    # ori_file：変換対象のファイルパス
+    # to_file ：変換後のファイルパス
 
+    wave, rate = rwave.read_wave(ori_file)
+    wave, _    = rwave.convert_fs(wave, rate, fs)
+    wave, _    = rwave.convert_sec(wave, rate, sec)
+
+    rwave.write_wave(to_file, wave, fs)
+
+
+convert_wave('tmp/1.wav', 'test.wav', 16000, 3)
 rate = 8000
 mfcc1 = rwave.to_mfcc('tmp/1.wav', rate)
 mfcc2 = rwave.to_mfcc('tmp/2.wav', rate)
