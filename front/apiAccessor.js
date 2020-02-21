cecd43d8b89d2d656e3b11ca5bdfb35965451d37
itanium <is0434ii@ed.ritsumei.ac.jp>
2020-02-21 23:02:27 +0900

    update

--- front/apiAccessor.js
+++ front/apiAccessor.js
@@ -1,54 +1,54 @@
-const APIURL = "http://172.31.115.20:3000/";
+const APIURL = "http://localhost:3000/";
 
 function api__voice_calc(sound) {
   let url = APIURL + 'voice/calc/1';
-  
+
 
   fetch(url, {
-    method: 'POST', // or 'PUT'
+    method: 'POST',
     body: sound,
-    headers:{
+    headers: {
       'Content-Type': 'audio/wav'
     }
   }).then(res => res.json())
     .then(response => {
       console.log('Success:', JSON.stringify(response));
       curScore = response.score;
-      document.querySelector("#score").innerHTML = String(curScore).substr(0,4);
+      document.querySelector("#score").innerHTML = String(curScore).substr(0, 4);
       showSec("result");
-      setTimeout(api__rank_post,1000);
+      setTimeout(api__rank_post, 1000);
     })
     .catch(error => console.error('Error:', error));
 }
 
 function api__rank_post() {
-  let name = prompt("君の名は？","名無しリッツ");
-  if(!name) return -1;
-  let url = APIURL + 'rank/'+curChar.id;
+  let name = prompt("君の名は？", "名無しリッツ");
+  if (!name) return -1;
+  let url = APIURL + 'rank/' + curChar.id;
   let data = {
     name: name,
     score: curScore
   };
 
   fetch(url, {
-    method: 'POST', // or 'PUT'
+    method: 'POST',
     body: JSON.stringify(data),
-    headers:{
+    headers: {
       'Content-Type': 'application/json'
     }
   });
 }
 
-function api__rank_get(){
-  let url = APIURL + 'rank/'+curChar.id;
+function api__rank_get() {
+  let url = APIURL + 'rank/' + curChar.id;
   fetch(url)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-    userRanking = myJson.ranks;
-    renderRanking();
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(JSON.stringify(myJson));
+      userRanking = myJson.ranks;
+      renderRanking();
+    }).catch(error => alert('[Error] Connection to Server Failed...\n\n'+ error));
 }
 
--- front/ranking.js
+++ front/ranking.js
@@ -1,52 +1,44 @@
-// const userRanking = [
-//     { ranking: 1 , userName: "てぃぁん", score: 90.28 },
-//     { ranking: 2, userName: "てぇあぃん", score: 83.28 },
-//     { ranking: 3, userName: "てやぁん", score: 63.13 },
-//     { ranking: 4, userName: "てあん", score: 50.42 },
-//     { ranking: 5, userName: "てぃやぁん", score: 20.28 },
-// ];
 let userRanking = [];
 
-function renderRanking(){
+function renderRanking() {
 
-let content = `
-<div class="rankingImg">
-<img src="${curChar.imgSrc}"/>
-</div>
-<div class="rankingStr">
-<p>${curChar.charName}</p>
-</div>
-`;
+  let content = `
+    <div class="rankingImg">
+    <img src="${curChar.imgSrc}"/>
+    </div>
+    <div class="rankingStr">
+    <p>${curChar.charName}</p>
+    </div>
+    `;
 
-for (let c of userRanking) {
+  for (let c of userRanking) {
     content += `
-    <div class="userRanking-list">
-     <p class="ranking">${c.rank}位</p>
-     <p class="name">${c.name}</p>
-     <p class="score2">${String(c.score).substr(0,4)}％</p>
-     </div>
-  `;
-}
+      <div class="userRanking-list">
+      <p class="ranking">${c.rank}位</p>
+      <p class="name">${c.name}</p>
+      <p class="score2">${String(c.score).substr(0, 4)}％</p>
+      </div>
+    `;
+  }
 
-content += (`<div class="otherRanking-list">`)
+  content += (`<div class="otherRanking-list">`)
 
-for(let c of chars){
-    if(c.charName == curChar.charName){
-        continue;
+  for (let c of chars) {
+    if (c.charName == curChar.charName) {
+      continue;
     }
-content += (`
+    content += (`
+      <div class="otherRanking-img">
+      <img onclick='curChar=${JSON.stringify(c)}; api__rank_get()' class="rankingImgSrc" src="${c.imgSrc}"/>
+      </div>
+    `);
+  }
 
-<div class="otherRanking-img">
-<img onclick='curChar=${JSON.stringify(c)}; api__rank_get()' class="rankingImgSrc" src="${c.imgSrc}"/>
-</div>
-`)
-}
+  content += (`</div>`);
 
-content += (`</div>`);
-
-document.querySelector("#rankingContent").innerHTML = content;
+  document.querySelector("#rankingContent").innerHTML = content;
 
 }
 
 curChar = chars[0];
-api__rank_get()
+api__rank_get();
